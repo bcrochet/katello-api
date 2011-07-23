d947776e7545822ba952a13579b60a387713d149
Dimitar Yordanov <dyordano@moonlight.brq.redhat.com>
2011-07-23 21:21:16 +0200

    Rewrite but keep the functionality

--- scripts/other/run-katello-api.sh
+++ scripts/other/run-katello-api.sh
@@ -1,58 +1,88 @@
-console(){
-        echo "*** $1"
+#!/bin/bash
+
+[[ $# -lt 1 ]] && printf "Usage: ./run-katello-api.sh katello.redhat.com [password]\n" && exit 1
+[[ ${USER} !=  'root' ]] && printf "You have to be root in order to run this script!\n" && exit 1
+
+########### Functions ###########
+function print_console() {
+    printf "### $1\n"  2>&1  |  tee -a $LOG_FILE  # Redirect Err and Out to $LOG_FILE
 }
 
-checkInstallRpm(){
-rpmName="$1"
-if [ "`rpm -q $rpmName | grep \"not installed\"`" != "" ]
-then 
-        console "Installing: [$rpmName] now"
-        yum -y install $rpmName > /dev/null 2>&1
-        console "[$rpmName]: `rpm -q $rpmName`"
-else
-        console "Using: [$rpmName]"
-fi
+function exit_code() {
+	print_console "###########################################################"
+    print_console "Test Log File = $LOG_FILE"
+    print_console "###########################################################"
+	exit $1 
 }
 
-# *****************************************************************************
-echo ""
+function InstallRpm(){
+	
+  [[ $# -ne  1 ]] && echo 'Usage: InstallRpm  $PKG_NAME' | tee -a $LOG_FILE  && return 1
+  	
+  local RpmPkg=$1
 
-checkInstallRpm git
-checkInstallRpm rubygems
-checkInstallRpm java-1.6.0-openjdk
-checkInstallRpm java-1.6.0-openjdk-devel
-checkInstallRpm ruby-devel
-checkInstallRpm gcc
+  if ! rpm -q ${RpmPkg} &> /dev/null  
+    then 
+        print_console "Installing: [${RpmPkg}] now"
+        yum -y install ${RpmPkg}  &>>  $LOG_FILE
+        ! rpm -q ${RpmPkg} &> /dev/null  && printf "FAILED to install ${RpmPkg}\n" | tee -a $LOG_FILE  && return 1
+        print_console "[${RpmPkg}]: $(rpm -q ${RpmPkg})"
+    else
+        print_console "Using: [${RpmPkg}]"
+  fi
+}
 
-JH="/usr/lib/jvm/`ls /usr/lib/jvm/ | grep java-1.6.0-openjdk | tail -1`" # to get java full path with `uname -i`
-export JAVA_HOME=$JH
-console "Exporting: [JAVA_HOME=$JH]"
+function InstallGem() {
+	
+  [[ $# -ne  1 ]] && echo 'Usage: InstallGem  $PKG_NAME' | tee -a $LOG_FILE  && return 1
+  local GemPkg=${1}
+  	
+  egrep  -qs 'gem: --no-ri --no-rdoc' $HOME/.gemrc ||  \
+     printf 'gem: --no-ri --no-rdoc\n' >> $HOME/.gemrc || exit_code 1 
 
-touch $HOME/.gemrc; echo "gem: --no-ri --no-rdoc" > $HOME/.gemrc
-if [ "`gem list | grep buildr | awk '{print $1}'`" != "buildr" ]
-then
-	console "Installing: [buildr]"
-	JAVA_HOME=$JH gem install buildr > /dev/null 2>&1 # IMPORTANT, all other dependencies seems would get installed :)
-fi
-console "Using: [`gem list | grep buildr`]"
+  if [[ $( gem list | awk '{print $1}' | egrep -c "^$GemPkg " ) -ne 1 ]]
+    then
+    	[[ $GemPkg == 'buildr' ]] && export JAVA_HOME=/usr/lib/jvm/java-openjdk && print_console "Exporting: [JAVA_HOME=${JAVA_HOME}]"
+     	print_console "Installing: [${GemPkg}]"
+        gem install ${GemPkg}  &>> $LOG_FILE 
+        print_console "${GemPkg}: [ $( gem list | egrep "^${GemPkg} " ) ]"
+    else     
+        print_console "Using: [ $( gem list | egrep '^buildr ' ) ]"
+  fi      
+	
+}
 
-SELF_DIR=$(dirname `readlink -f $0`)
-pushd $SELF_DIR/../../ > /dev/null
+### Script START 
 
 # running the tests
-KATELLO_SERVER_HOSTNAME="$1"
-KATELLO_SSH_PASS="$2"
-cp ./katello-automation.properties ./automation.properties
-sed -i '/katello.server.hostname/ d' ./automation.properties
-echo "katello.server.hostname=$KATELLO_SERVER_HOSTNAME" >> ./automation.properties
-sed -i '/katello.server.hostname/ d' ./automation.properties
-echo "katello.server.hostname=$KATELLO_SERVER_HOSTNAME" >> ./automation.properties
-sed -i '/katello.ssh.passphrase/ d' ./automation.properties
-echo "katello.ssh.passphrase=$KATELLO_SSH_PASS" >> ./automation.properties
+DEFAULT_SERVER_ROOT_PASS='redhat'
+KATELLO_SERVER_HOSTNAME=${1}
+KATELLO_SSH_PASS=${2:-$DEFAULT_SERVER_ROOT_PASS}
 
-JAVA_OPTS="-Dautomation.propertiesfile=`pwd`/automation.properties" KATELLO_API_TESTNAMES="ALl_Tests" buildr compile test --trace
-
-echo ""
+DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+LOG_FILE=/tmp/api-test-${DATE}.log
 
 
+InstallRpm git || exit_code 1 
+InstallRpm rubygems ||  exit_code 1
+InstallRpm java-1.6.0-openjdk || exit_code 1
+InstallRpm java-1.6.0-openjdk-devel || exit_code 1
+InstallRpm ruby-devel || exit_code 1
+InstallRpm gcc || exit_code 1
 
+InstallGem buildr || exit_code 1
+
+SELF_DIR=$(dirname `readlink -f $0`)
+pushd ${SELF_DIR}/../../ > /dev/null
+
+cp ./katello-automation.properties /tmp/automation.properties
+sed -i "s/^\([ ]*katello.server.hostname[ ]*=\).*/\1${KATELLO_SERVER_HOSTNAME}/" /tmp/automation.properties
+sed -i "s/^\([ ]*katello.ssh.passphrase[ ]*=\).*/\1${KATELLO_SSH_PASS}/" /tmp/automation.properties
+
+
+# Send detailed output to LOG_FILE
+JAVA_OPTS="-Dautomation.propertiesfile=/tmp/automation.properties" KATELLO_API_TESTNAMES="ALl_Tests" buildr compile test --trace  2>>$LOG_FILE |  tee -a $LOG_FILE
+ 
+exit_code 0;
+
+### Script END
