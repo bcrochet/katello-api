3e628b510532977c178528d5d312f8413162eff8
Garik Khachikyan <gkhachik@redhat.com>
2011-09-16 16:17:16 +0200

    1 more test using dataProvider

--- src/com/redhat/qe/katello/tests/cli/OrgTests.java
+++ src/com/redhat/qe/katello/tests/cli/OrgTests.java
@@ -3,6 +3,7 @@ package com.redhat.qe.katello.tests.cli;
 import com.redhat.qe.auto.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.redhat.qe.katello.base.KatelloCliDataProvider;
 import com.redhat.qe.katello.base.KatelloCliTestScript;
 import com.redhat.qe.tools.SSHCommandResult;
 
@@ -15,4 +16,23 @@ public class OrgTests extends KatelloCliTestScript{
 		Assert.assertEquals(res.getExitCode().intValue(), 0, "Check - return code");
 		Assert.assertTrue(res.getStdout().contains(ACME_ORG), "Check - contains: ["+ACME_ORG+"]");
 	}
+	
+	@Test(groups = {"cli-org"}, 
+			description = "Create an org - different variations",
+			dataProviderClass = KatelloCliDataProvider.class,
+			dataProvider = "org_create")
+	public void test_createOrg(String name, String descr){
+		String cmd = "org create";
+		if(name != null || descr != null){
+			if (name != null)
+				cmd = cmd +" --name "+name;
+			if (descr != null)
+				cmd = cmd +" --description "+descr;
+		}
+		SSHCommandResult res = clienttasks.run_cliCmd(cmd);
+		Assert.assertEquals(res.getExitCode().intValue(), 0, "Check - return code");
+	}
+	
+	
+	
 }
