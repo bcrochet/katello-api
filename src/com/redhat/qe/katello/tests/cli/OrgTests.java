0b55450991fb1888d484b6e34a7d80a8e2a5da36
Garik Khachikyan <gkhachik@redhat.com>
2011-09-26 12:05:34 +0200

    added check: default ACME_Coproation existence.

--- src/com/redhat/qe/katello/tests/cli/OrgTests.java
+++ src/com/redhat/qe/katello/tests/cli/OrgTests.java
@@ -11,14 +11,14 @@ import com.redhat.qe.tools.SSHCommandResult;
 
 public class OrgTests extends KatelloCliTestScript{
 	Vector<String[]> orgs;
-//	@Test(groups = {"cli-org"}, 
-//			description = "List all orgs - ACME_Corporation should be there")
-//	public void test_listOrgs(){
-//		SSHCommandResult res = clienttasks.run_cliCmd("org list");
-//		Assert.assertEquals(res.getExitCode().intValue(), 0, "Check - return code");
-//		Assert.assertTrue(res.getStdout().contains(ACME_ORG), "Check - contains: ["+ACME_ORG+"]");
-//	}
-//	
+	@Test(groups = {"cli-org"}, 
+			description = "List all orgs - ACME_Corporation should be there")
+	public void test_listOrgs_ACME_Corp(){
+		SSHCommandResult res = clienttasks.run_cliCmd("org list");
+		Assert.assertEquals(res.getExitCode().intValue(), 0, "Check - return code");
+		Assert.assertTrue(res.getStdout().contains(ACME_ORG), "Check - contains: ["+ACME_ORG+"]");
+	}
+	
 	@Test(groups = {"cli-org"}, 
 			description = "Create org - different variations",
 			dataProviderClass = KatelloCliDataProvider.class,
