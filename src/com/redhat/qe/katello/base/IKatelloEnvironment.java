f6fd6da290646e4d2af6bd14f01d40b6babfad25
Garik Khachikyan <gkhachik@redhat.com>
2011-10-26 10:49:00 +0200

    Adding Katello cli environments interface

---
+++ src/com/redhat/qe/katello/base/IKatelloEnvironment.java
@@ -0,0 +1,7 @@
+package com.redhat.qe.katello.base;
+
+public interface IKatelloEnvironment {
+
+	public static final String LOCKER = "Locker";
+	public static final String CREATE_NODESC = "environment create --org \"%s\" --name \"%s\" --prior \"%s\"";
+}
