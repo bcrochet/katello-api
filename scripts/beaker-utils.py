3235159c7499a683853ca9e7ed3d0719e3d87c64
Garik Khachikyan <gkhachik@redhat.com>
2011-12-06 09:04:04 +0100

    commments: howToUse

--- scripts/beaker-utils.py
+++ scripts/beaker-utils.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
-
+'''
+How to use:
+wget http://git.engineering.redhat.com/?p=users/gkhachik/katello-api.git;a=blob_plain;f=scripts/beaker-utils.py;hb=HEAD -O beaker-utils.py
+chmod a+x beaker-utils.py
+./beaker-utils.py <methodname>
+'''
 
