362dfc2e35f0b294a0a8442e2b18e64b9d1bbf50
Garik Khachikyan <gkhachik@redhat.com>
2011-12-07 14:26:12 +0100

    added: getBeakerJobStatus

--- scripts/beaker-utils.py
+++ scripts/beaker-utils.py
@@ -4,9 +4,20 @@ How to use:
 wget http://git.engineering.redhat.com/?p=users/gkhachik/katello-api.git;a=blob_plain;f=scripts/beaker-utils.py;hb=HEAD -O beaker-utils.py
 chmod a+x beaker-utils.py
 ./beaker-utils.py --method=<methodname> [--args=arg1=value1,arg2=value2,<etc>]
+
+===
+NOTES:
+# Returns Beaker JobID (if machine is reserved through Beaker)
+(cat /etc/motd | grep JOBID | cut -f2 -d=)
+
+# stores Jenkins node dsa public key:
+(export JENKINS_DSA_PUBKEY=`cat ~/.ssh/id_dsa.pub`)
 '''
 from optparse import OptionParser
 import os
+from xml.etree.ElementTree import ElementTree
+from subprocess import Popen
+import subprocess
 
 def parseDictFromArgs(sargs, delimiter=',', assignOperator='='):
     arr = sargs.split(delimiter)
@@ -30,7 +41,19 @@ class BeakerUtils(object):
             print "http://download.eng.tlv.redhat.com/pub/fedora/linux/releases/15/Fedora/x86_64/os/"
         else: # BRQ; RDU
             print "http://download.eng.brq.redhat.com/pub/fedora/linux/releases/15/Fedora/x86_64/os/"
-
+    
+    def getBeakerJobStatus(self, jid):
+        Popen("bkr job-results %s > /tmp/beaker-%s.log"%(jid,jid), stdout=None, shell=True).wait()
+        
+        et = ElementTree()
+        tree = ElementTree.parse(et,"/tmp/beaker-%s.log"%jid)
+        tasks = tree.findall("recipeSet/recipe/task")
+        for task in tasks:
+            print "name=[%s]; result=[%s]; duration=[%s]; status=[%s]"%\
+            (task.get("name"),task.get("result"),task.get("duration"),task.get("status"))
+            
+        
+    
 def main():
     parser = OptionParser()
     parser.add_option("--method", dest="method", 
