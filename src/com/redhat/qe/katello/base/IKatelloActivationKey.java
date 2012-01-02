8d189e340368d668b56ea5377bc2b101135bb56d
Garik Khachikyan <gkhachik@redhat.com>
2012-01-02 14:41:15 +0100

    added: _TEMPLATE

--- src/com/redhat/qe/katello/base/IKatelloActivationKey.java
+++ src/com/redhat/qe/katello/base/IKatelloActivationKey.java
@@ -3,4 +3,9 @@ package com.redhat.qe.katello.base;
 public interface IKatelloActivationKey {
 
 	public static final String CREATE_NODESC = "activation_key create --org \"%s\" --environment \"%s\" --name \"%s\"";
+	public static final String CREATE_NODESC_TEMPLATE = "activation_key create --org \"%s\" --environment \"%s\" --name \"%s\" --template \"%s\"";
+	
+	public static final String ERR_TEMPLATE_NOTFOUND = 
+			"Could not find template [ %s ]";
+	
 }
