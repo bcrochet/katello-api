33da37096e25810e17706dbb24eea6f2118f5530
Garik Khachikyan <gkhachik@redhat.com>
2012-01-16 13:01:55 +0100

    new testcase

--- src/com/redhat/qe/katello/tests/cli/UserTests.java
+++ src/com/redhat/qe/katello/tests/cli/UserTests.java
@@ -11,7 +11,7 @@ import com.redhat.qe.tools.SSHCommandResult;
 public class UserTests extends KatelloCliTestScript{
 
 	static{
-//		 System.setProperty("katello.cli.reuseSystem", "true");  // TODO - /me needs to be commented.
+		 System.setProperty("katello.cli.reuseSystem", "true");  // TODO - /me needs to be commented.
 	}
 	
 	@Test(description="create user - for default org", enabled=true)
@@ -33,4 +33,23 @@ public class UserTests extends KatelloCliTestScript{
 	}
 	
 	// TODO - with dataProvider provide more variations of user names in create action.
+	
+	@Test(description="create user - for default org (disabled)", enabled=true)
+	public void test_createDisabled_DefaultOrg(){
+		SSHCommandResult res;
+		String uniqueID = KatelloTestScript.getUniqueID();
+		String username = "disabled-"+uniqueID;
+		String userpass = "password";
+		String usermail = username+"@localhost";
+		
+		KatelloUser usr = new KatelloUser(clienttasks, username, usermail, userpass, true);
+		res = usr.create();
+		Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code ("+KatelloUser.CMD_CREATE+")");
+		Assert.assertTrue(res.getStdout().trim().contains(
+				String.format(KatelloUser.OUT_CREATE,username)), 
+				"Check - returned output string ("+KatelloUser.CMD_CREATE+")");
+		
+		usr.asserts_create();
+	}
+	
 }
