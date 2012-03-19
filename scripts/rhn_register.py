59af8cc9d0db7a059abb1a31bc20d5f5da1cfb5e
Garik Khachikyan <gkhachik@redhat.com>
2012-03-19 14:36:05 +0100

    adding IS_CFSE

--- scripts/rhn_register.py
+++ scripts/rhn_register.py
@@ -7,12 +7,15 @@ EUS_CHANNEL = "RHEL EUS Server Optional ("
 URL = "https://rhn.errata.stage.redhat.com/rpc/api"
 user = os.getenv("RHN_USER","qa@redhat.com")
 pswd = os.getenv("RHN_PASS","redhatqa")
+is_cfse = os.getenv("IS_CFSE","false")
 
 # Register to RHN
 print os.popen("rhnreg_ks --username %s --password %s --use-eus-channel --force"%(user,pswd)).read()
 systemid = os.popen("cat /etc/sysconfig/rhn/systemid | grep 'ID-' | awk -F 'ID-' '{print $2}' | awk -F '<' '{print $1}'").read()
 print "Registered with ID:", systemid
 
+if is_cfse == "true": # is CFSE - no Optional channel subscription needed.
+   return
 client = xmlrpclib.Server(URL, verbose=0)
 session = client.auth.login(user, pswd)
 list = client.system.listChildChannels(session,int(systemid))
