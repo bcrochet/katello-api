61801a27cc809ef6fdd72abb5274cdc507cd8323
Garik Khachikyan <gkhachik@redhat.com>
2012-03-27 10:20:10 +0200

    TDL export: Fedora

--- src/com/redhat/qe/katello/tests/e2e/TdlExportFedora.java
+++ src/com/redhat/qe/katello/tests/e2e/TdlExportFedora.java
@@ -1,7 +1,5 @@
 package com.redhat.qe.katello.tests.e2e;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.logging.Logger;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -22,16 +20,20 @@ import com.redhat.qe.tools.SSHCommandResult;
 
 /**
  * Implementation of one of V1 scenarios:<BR>
- * Scenario: https://tcms.engineering.redhat.com/case/123748/?from_plan=4785<BR>
+ * Scenario: https://tcms.engineering.redhat.com/case/123749/?from_plan=4785<BR>
  * Description:<BR>
- * TDL export from RHEL repo 
+ * TDL export from Fedora repo 
  * @author gkhachik
  */
-public class TdlExportRhel extends KatelloCliTestScript{
-	protected static Logger log = Logger.getLogger(TdlExportRhel.class.getName());
+public class TdlExportFedora extends KatelloCliTestScript{
+	protected static Logger log = Logger.getLogger(TdlExportFedora.class.getName());
+	public static final String FEDORA_VER = "16";
 	public static final String TDL_AEOLUS_VALIDATOR = "https://raw.github.com/aeolusproject/conductor/master/src/app/util/template-rng.xml";
 
 	String org;
+	String provider;
+	String product;
+	String repo;
 	private String env_prod;
 	private String cs_prod;
 	private String template_prod;
@@ -42,57 +44,37 @@ public class TdlExportRhel extends KatelloCliTestScript{
 	@BeforeTest(description="Init unique names", alwaysRun=true)
 	public void setUp(){
 		String uid = KatelloTestScript.getUniqueID();
+		this.provider = "Fedora_"+uid;
+		this.product = "Fedora_"+uid;
+		this.repo = "Fedora"+FEDORA_VER+"_"+uid;
+		
 		this.env_prod = "Prod-"+uid;
-		this.template_prod = "template_RHEL_"+uid;
+		this.template_prod = "template_Fedora_"+uid;
 		this.cs_prod = "cs_"+this.env_prod;
 
-		ArrayList<String> orgs = getOrgsWithImportedManifest();
-		log.info("E2E - check org to use/create for importing manifest");
-		if(orgs.size()==0){
-			log.info("Seems there is no org with imported stage manifest. Doing it now");
-			SCPTools scp = new SCPTools(
-					System.getProperty("katello.client.hostname", "localhost"), 
-					System.getProperty("katello.client.ssh.user", "root"), 
-					System.getProperty("katello.client.sshkey.private", ".ssh/id_hudson_dsa"), 
-					System.getProperty("katello.client.sshkey.passphrase", "secret"));
-			Assert.assertTrue(scp.sendFile("data"+File.separator+"export.zip", "/tmp"),
-					"export.zip sent successfully");			
-			this.org = "TDL-RHEL-"+uid; // TODO - do org with space. Now there is a bug.
-			KatelloOrg org = new KatelloOrg(clienttasks, this.org, null);
-			org.create();
-			KatelloProvider prov = new KatelloProvider(clienttasks, KatelloProvider.PROVIDER_REDHAT, this.org, null, null);
-			SSHCommandResult res = prov.import_manifest("/tmp"+File.separator+"export.zip", new Boolean(true));
-			Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (provider import_manifest)");
-			Assert.assertTrue(res.getStdout().trim().contains("Manifest imported"),"Message - (provider import_manifest)");
-		}else{
-			this.org = orgs.get(0);
-			log.info("There is an org having manifest. Using: ["+this.org+"]");
-		}		
-		log.info("Enable repo: ["+KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT+"]");
-		KatelloRepo repo = new KatelloRepo(clienttasks, KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT, this.org, KatelloProduct.RHEL_SERVER, null, null, null);
-		SSHCommandResult res = repo.enable();
-		Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (repo enable)");
-		Assert.assertTrue(res.getStdout().trim().contains("enabled."),"Message - (repo enable)");
+		this.org = "TDL-Fedora-"+uid;
+		KatelloOrg org = new KatelloOrg(clienttasks, this.org, null);
+		org.create();
+		KatelloProvider prov = new KatelloProvider(clienttasks, this.provider, this.org, null, null);
+		prov.create();
+		KatelloProduct prod = new KatelloProduct(clienttasks, this.product, this.org, this.provider, null, null, null, null, null);
+		prod.create();
+		String fedoraUrl = getFedoraUrl(); // <--- here goes URL
+		KatelloRepo repo = new KatelloRepo(clienttasks, this.repo, this.org, this.product, fedoraUrl, null, null);
+		repo.create();
 	}
 	
-	@Test(description="Sync RHEL repo - if it's not", enabled=true)
+	@Test(description="Sync Fedora repo", enabled=true)
 	public void test_syncRepo(){
-		log.info("E2E - sync repo (if needed");
-		KatelloRepo repo = new KatelloRepo(clienttasks, KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT, this.org, KatelloProduct.RHEL_SERVER, null, null, null);
-		SSHCommandResult res = repo.info();
+		log.info("E2E - sync Fedora "+FEDORA_VER+" repo");
+		KatelloRepo repo = new KatelloRepo(clienttasks, this.repo, this.org, this.product, null, null, null);
+		SSHCommandResult res = repo.synchronize();
+		Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (repo synchronize)");
+		res = repo.info();
 		int pkgCount = Integer.parseInt(KatelloCliTasks.grepCLIOutput("Package Count", res.getStdout()));
-		if(pkgCount==0){
-			log.info("Seems repo is not synchronized yet. Doing now ...");
-			res = repo.synchronize();
-			Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (repo synchronize)");
-			res = repo.info();
-			pkgCount = Integer.parseInt(KatelloCliTasks.grepCLIOutput("Package Count", res.getStdout()));
-			String progress = KatelloCliTasks.grepCLIOutput("Progress", res.getStdout());
-			Assert.assertTrue(pkgCount>0, "Check - Packages >0");
-			Assert.assertTrue(progress.equals("Finished"), "Check: status of repo sync - Finished");
-		}else{
-			log.info("Seems repo is synchronized. Found ["+pkgCount+"] there");
-		}
+		String progress = KatelloCliTasks.grepCLIOutput("Progress", res.getStdout());
+		Assert.assertTrue(pkgCount>0, "Check - Packages >0");
+		Assert.assertTrue(progress.equals("Finished"), "Check: status of repo sync - Finished");
 	}
 	
 	@Test(description="Prepare changeset", dependsOnMethods={"test_syncRepo"}, enabled=true)
@@ -102,8 +84,7 @@ public class TdlExportRhel extends KatelloCliTestScript{
 		env.create();
 		KatelloChangeset cs = new KatelloChangeset(clienttasks, this.cs_prod, this.org, this.env_prod);
 		cs.create();
-		SSHCommandResult res = cs.update_addProduct(KatelloProduct.RHEL_SERVER);
-		//SSHCommandResult res = cs.update_fromProduct_addRepo(KatelloProduct.RHEL_SERVER, KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT);
+		SSHCommandResult res = cs.update_addProduct(this.product);
 		Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (changeset add_product)");
 	}
 	
@@ -111,13 +92,13 @@ public class TdlExportRhel extends KatelloCliTestScript{
 	public void test_prepareTemplate(){
 		KatelloTemplate tpl = new KatelloTemplate(clienttasks, this.template_prod, null, this.org, null);
 		tpl.create();
-		KatelloDistribution dist = new KatelloDistribution(clienttasks, this.org, KatelloProduct.RHEL_SERVER, 
-				KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT, null);
+		KatelloDistribution dist = new KatelloDistribution(
+				clienttasks, this.org, this.product,this.repo, null);
 		SSHCommandResult res = dist.list();
 		this.distribution = KatelloCliTasks.grepCLIOutput("Id", res.getStdout());
 		Assert.assertTrue((this.distribution!=null), "Check - distribution exists in repo");
-		tpl.update_add_repo(KatelloProduct.RHEL_SERVER, KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT);
-		res = tpl.update_add_distribution(KatelloProduct.RHEL_SERVER, this.distribution);
+		tpl.update_add_repo(this.product, this.repo);
+		res = tpl.update_add_distribution(this.product, this.distribution);
 		Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (template add_distribution)");
 		tpl.update_add_package("vim-enhanced");
 		tpl.update_add_package_group("Desktop");
@@ -154,7 +135,6 @@ public class TdlExportRhel extends KatelloCliTestScript{
 				System.getProperty("katello.client.sshkey.private", ".ssh/id_hudson_dsa"), 
 				System.getProperty("katello.client.sshkey.passphrase", "secret"));
 		
-		
 		Assert.assertTrue(scp.getFile("/tmp/"+this.templateName, "data/"),
 				"TDL export file get - sucessfully");
 		KatelloCliTasks.run_local(false, "rm -rf /tmp/template-rng.xml; wget "+TDL_AEOLUS_VALIDATOR+" -O /tmp/template-rng.xml");
@@ -168,10 +148,31 @@ public class TdlExportRhel extends KatelloCliTestScript{
 		String pemKey = System.getProperty("user.dir")+"/data/key-"+this.org+".pem";
 		String pemCert = System.getProperty("user.dir")+"/data/cert-"+this.org+".pem";
 		KatelloCliTasks.run_local(true, "scripts/katello-utils.py --method tdl_extract_certs --args " +
-				"\"filename=data/"+this.templateName+",reponame="+KatelloRepo.RH_REPO_RHEL6_SERVER_RPMS_64BIT+",certname="+pemCert+",keyname="+pemKey+"\"");
+				"\"filename=data/"+this.templateName+",reponame="+this.repo+",certname="+pemCert+",keyname="+pemKey+"\"");
 		String url = KatelloCliTasks.run_local(true, "scripts/katello-utils.py --method tdl_fromTag --args " +
 				"\"filename=data/"+this.templateName+",tag=os/install/url\"");
 		String res = KatelloCliTasks.run_local(true, "curl -sk --cert "+pemCert+" --key "+pemKey+" \""+url+"/repodata/repomd.xml\" | grep \"<location href=\\\"repodata/.*filelists.xml.gz\\\"/>\" | wc -l");
 		Assert.assertTrue(res.equals("1"), "Check - could be able to get repomd.xml content unlocked.");
+	}
+	
+	private String getFedoraUrl(){
+		String domain="";
+		
+		String lab_controller = servertasks.execute_remote("echo ${LAB_CONTROLLER}").getStdout().trim();
+		if(lab_controller.equals("")) lab_controller = "lab.rhts.englab.brq.redhat.com";
+		
+		if(lab_controller.equals("lab2.rhts.eng.bos.redhat.com"))
+			domain = "download.bos.redhat.com";
+		if(lab_controller.equals("lab.rhts.eng.nay.redhat.com"))
+			domain = "download.eng.nay.redhat.com";
+		if(lab_controller.equals("lab.rhts.eng.pnq.redhat.com"))
+			domain = "download.eng.pnq.redhat.com";
+		if(lab_controller.equals("lab-01.eng.tlv.redhat.com"))
+			domain = "download.eng.tlv.redhat.com";
+		if(lab_controller.equals("lab.rhts.englab.brq.redhat.com"))
+			domain = "download.eng.brq.redhat.com";
+
+		String _url = "http://"+domain+"/pub/fedora/linux/releases/"+FEDORA_VER+"/Fedora/x86_64/os/";
+		return _url;
 	}
 }
